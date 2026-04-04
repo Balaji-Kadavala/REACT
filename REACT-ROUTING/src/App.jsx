d03241b0967ac35e2342d0f997b92d7cef33b3fa
BALAJI KADAVALA <balaji.kadavala@gmail.com>
2026-04-04 11:33:20 +0530

    Routing Updated

--- REACT-ROUTING/src/App.jsx
+++ REACT-ROUTING/src/App.jsx
@@ -1,13 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
-import Home from "./features/Home";
 function App()
 {
   return (<div>
     <h1>React Routing</h1>
-    <Link to="/home">Home</Link>
-    <Link to="/dashboard">Dashboard</Link>
-    <Link to="/about">About</Link>
-    <Link to="/contactUs">Contact Us</Link>
+    <Link to="home">Home</Link>
+    <Link to="dashboard">Dashboard</Link>
+    <Link to="about">About</Link>
+    <Link to="contactUs">Contact Us</Link>
     <Outlet></Outlet>
   </div>)
 }
